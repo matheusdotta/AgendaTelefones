65687893c7c301d8db0c5cdec5bd92531c770374
Matheus Dotta <50276529+matheusdotta@users.noreply.github.com>
2021-03-01 18:25:50 -0300

    Initial commit

---
+++ src/Agenda/Main.java
@@ -0,0 +1,20 @@
+package Agenda;
+
+import java.io.IOException;
+
+/**
+ * Classe que lança a aplicação
+ */
+public class Main {
+
+	/**
+	 * Método main
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		// Instancia a aplicação e a inicia
+		Aplicacao aplicacao = new Aplicacao();
+		aplicacao.iniciar();
+	}
+}
